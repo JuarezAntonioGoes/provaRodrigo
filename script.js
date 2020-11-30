25a4f3a5c0d29c569faf3842a591d8b48cc286ff
JuarezGoesJunior <juarezantoniog12@gmail.com>
2020-11-30 20:15:28 -0300

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let saldoTotal = 0
+
+
 document.querySelector('#salvar')
     .addEventListener('click', newLineTable)
 
@@ -8,14 +11,54 @@ function newLineTable(e) {
     tableBody.insertAdjacentElement('afterbegin', tableTR)
     const selectTH = tableBody.querySelector('tr')
 
+    let verifyDespesa = false
+
+    let newValor = 0;
+
+    const tipo = document.querySelector('#tipo')
+    if (tipo.value.toLowerCase() === 'despesa') {
+        // console.log('oi');
+        let valor = +document.querySelector('#valor').value
+        newValor = valor - (valor * 2)
+        // console.log(valor);
+        verifyDespesa = true
+    }
+
+
+
+
     const inputsForm = document.querySelectorAll('input[type="text"]')
 
-        .forEach(campo => {
-            
+        .forEach((campo, index) => {
 
-            selectTH.innerHTML += `<td>${campo.value}</td>`
+            if (verifyDespesa && index === 1) {
+                selectTH.innerHTML += `<td>${newValor}</td>`
+            } else {
+                selectTH.innerHTML += `<td>${campo.value}</td>`
+            }
+
         })
 
-    console.log(tableBody);
+    const EveryTipo = document.querySelectorAll('#tipo')
+    
+    document.querySelectorAll('#valor')
+        .forEach((valor, index) => {
+            
+
+            
+            if (verifyDespesa && EveryTipo[index].value.toLowerCase() === 'despesa') {
+                saldoTotal -= Number(valor.value)
+
+            } else {
+                saldoTotal += Number(valor.value)
+            }
+
+            // console.log(saldoTotal);
+        })
+
+    // console.log(tableBody);
+
+    const resultSaldo = document.querySelector('#resultTable')
+    resultSaldo.innerText = `Saldo R$ ${saldoTotal}`
 
 }
